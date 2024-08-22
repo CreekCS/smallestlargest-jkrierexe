1557831e7e21662a114de4e6c1da73a69efc6169
Jackson Krier <jackson.krier@gmail.com>
2024-08-22 02:51:43 +0000

    maybe done

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,16 +1,33 @@
-Jackson Krier
+//Jackson Krier
 public class SmallestLargest{
     public static void main(String[] args){
         int[] nums = new int[10];
         for(int i=0; i<nums.length; i++){
-            nums[i] = 
+            nums[i] = (int)(Math.random()*100);
         }
+        System.out.println(smallestValue(nums));
+        System.out.println(largestValue(nums));
 
     }
-    public static int smallestValue(){
+    public static int smallestValue(int[] nums){
+        int min = nums[0];
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]<min){
+                min = nums[i];
+            }
+        }
+        return min;
+
 
     }
-    public static int largestValue(){
+    public static int largestValue(int[] nums){
+        int max = nums[0];
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]> max){
+                max = nums[i];
+            }
+        }
+        return max;
         
     }
 }
